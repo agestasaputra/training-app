349d555b022583d93c17ea1e3253626d7ae2be68
Agesta Saputra <agesta.budy@mekari.com>
2022-03-06 14:15:44 +0700

    adjustment: update copyright year to 2022 on footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
       <nuxt />
     </div>
     <footer class="bg-primary text-light fixed-bottom d-flex justify-content-center align-item-center p-4">
-      <p class="m-0">Copyright 2017 | built by &hearts;</p>
+      <p class="m-0">Copyright 2022 | built by &hearts;</p>
     </footer>
   </div>
 </template>
