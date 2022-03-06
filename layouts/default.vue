fef1a6d62b970e0eb45a10c676e7c8e6292c35a9
Agesta Saputra <agesta.budy@mekari.com>
2022-03-06 13:25:42 +0700

    feat: add sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="q-layout">
+    <!-- Sidebar -->
+    <b-sidebar v-model="sidebar.isShown" id="sidebar-no-header" aria-labelledby="sidebar-no-header-title" no-header shadow>
+      <template>
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Custom header sidebar</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </p>
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="hide">Active</b-nav-item>
+              <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+            </b-nav>
+          </nav>
+          <b-button variant="primary" block @click="sidebar.isShown = !sidebar.isShown">Close Sidebar</b-button>
+        </div>
+      </template>
+    </b-sidebar>
     <!-- Main Content -->
     <div class="q-layout__page">
       <nuxt />
@@ -8,5 +28,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      sidebar: {
+        isShown: true
+      }
+    }
+  }
+}
 </script>
